079b0322609631d0fd36cbcd29d86b797851a529
Michael de Groot <m.degroot@nl.ccv.eu>
2018-09-17 11:16:18 +0200

    Small bugfixes
    - Fixed profile directories with spaces (i.e. "John Doe")
    - Fixed wscript.exe not in path by specifying path with
    %windir%\system32

--- run_app_no_console.bat
+++ run_app_no_console.bat
@@ -1,9 +1,9 @@
 @echo off
 
 echo @echo off > tmp.bat
-echo %cd%\run_app.bat %* >> tmp.bat
+echo "%cd%"\run_app.bat %* >> tmp.bat
 echo exit >> tmp.bat
 
-wscript.exe "%cd%\invisible.vbs" "%cd%\tmp.bat" 
+%windir%\system32\wscript.exe "%cd%\invisible.vbs" "%cd%\tmp.bat"
 
 exit
